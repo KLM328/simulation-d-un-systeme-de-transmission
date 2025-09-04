ed2d4def00c68c478b4085d62ca412c674d99d9e
helena <helena@10.129.166.163>
2025-09-04 11:09:18 +0200

    Constructeur SourceFixe

--- src/sources/SourceFixe.java
+++ src/sources/SourceFixe.java
@@ -2,8 +2,20 @@ package sources;
 
 import information.Information;
 
-public class SourceFixe extends Source{
+public class SourceFixe extends Source<Boolean>{
 
+	 /**
+     * Une source qui envoie toujours le même message
+     */
+	public SourceFixe() {
+		informationGeneree = new Information<Boolean>();
+        informationGeneree.add(true);
+        informationGeneree.add(false);
+        informationGeneree.add(true);
+        informationGeneree.add(true);
+        informationGeneree.add(false);
+        informationGeneree.add(true); 
+	}
 	
 	public void recevoir(Information<Boolean> info) {
 		
