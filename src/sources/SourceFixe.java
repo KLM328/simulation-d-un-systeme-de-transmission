e314f2a7828d9025465b7870e446364ed9c4bb5f
helena <helena@10.129.166.163>
2025-09-04 11:06:25 +0200

    Ajout dossiers src, bin, docs

---
+++ src/sources/SourceFixe.java
@@ -0,0 +1,19 @@
+package sources;
+
+import information.Information;
+
+public class SourceFixe extends Source{
+
+	
+	public void recevoir(Information<Boolean> info) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
